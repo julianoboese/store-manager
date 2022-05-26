4a9070e89cdf4e393b7c8b7de46f5f7afc57f138
Juliano Boese <juliano.boese@gmail.com>
2022-05-26 16:13:50 -0300

    test(prodMod): :test_tube: add failing tests for getProducts model function
    req1

---
+++ test/unit/models/productsModel.js
@@ -0,0 +1,42 @@
+const { expect } = require("chai");
+const sinon = require('sinon');
+const connection = require("../../../db/connection");
+
+const ProductsModel = {
+  getProducts: () => {}
+}
+
+const productsData = [[
+  {
+    id: 1,
+    name: 'product A',
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: 'product B',
+    quantity: 20
+  }
+]];
+
+describe('The getProducts Model function', () => {
+  before(async () => {
+    sinon.stub(connection, 'execute').resolves(productsData);
+  })
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  it('returns an array', async () => {
+    const products = await ProductsModel.getProducts();
+
+    expect(products).to.be.an('array')
+  })
+
+  it('returns all products', async () => {
+    const products = await ProductsModel.getProducts();
+
+    expect(products).to.equal(productsData[0])
+  })
+})

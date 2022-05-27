f87cdc7e79c82a9e62bf9ddbec4ceea6078eb7ca
Juliano Boese <juliano.boese@gmail.com>
2022-05-27 00:20:34 -0300

    test(prodMod): :test_tube: add failing tests for getProduct model function
    req1

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,11 +1,18 @@
 const connection = require('../db/connection');
 
 async function getProducts() {
-  const [products] = await connection.execute('SELECT * FROM StoreManager.products');
+  const [products] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
+  );
 
   return products;
 }
 
+async function getProduct(id) {
+  return id;
+}
+
 module.exports = {
   getProducts,
+  getProduct,
 };
--- test/unit/models/productsModel.js
+++ test/unit/models/productsModel.js
@@ -37,3 +37,62 @@ describe('The getProducts Model function', () => {
     expect(products).to.equal(productsData[0])
   })
 })
+
+describe('The getProduct Model function', () => {
+
+  describe('when there is a product with the selected id', () => {
+    const productData = [[
+      {
+        id: 1,
+        name: 'product A',
+        quantity: 10
+      },
+    ]];
+  
+    const id = 1;
+  
+    before(() => {
+      sinon.stub(connection, 'execute').resolves(productData);
+    })
+  
+    after(() => {
+      connection.execute.restore();
+    });
+  
+    it('returns an object', async () => {
+      const product = await ProductsModel.getProduct(id);
+  
+      expect(product).to.be.an('object');
+      expect(Object.keys(product)).to.have.lengthOf(3);
+      expect(product).to.have.property('id').that.is.a('number');
+      expect(product).to.have.property('name').that.is.a('string');
+      expect(product).to.have.property('quantity').that.is.a('number');
+    })
+  
+    it('returns the product with the selected id', async () => {
+      const product = await ProductsModel.getProduct(id);
+  
+      expect(product).to.equal(productData[0][0])
+    })
+  })
+
+  describe('when there is no product with the selected id', () => {
+    const productData = [[]];
+  
+    const id = 4;
+  
+    before(() => {
+      sinon.stub(connection, 'execute').resolves(productData);
+    })
+  
+    after(() => {
+      connection.execute.restore();
+    });
+  
+    it('returns undefined', async () => {
+      const product = await ProductsModel.getProduct(id);
+  
+      expect(product).to.be.undefined;
+    })
+  })
+})

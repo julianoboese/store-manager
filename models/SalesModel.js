00b0fba2feee57fc04f9bbe4b04bd5211cae62bb
Juliano Boese <juliano.boese@gmail.com>
2022-05-27 23:47:35 -0300

    test(salesServ): :test_tube: add failing tests for getSale service function
    req1

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -19,6 +19,11 @@ async function getSales() {
   return sales;
 }
 
+async function getSale(id) {
+  return id;
+}
+
 module.exports = {
   getSales,
+  getSale,
 };
--- test/unit/services/salesService.js
+++ test/unit/services/salesService.js
@@ -39,3 +39,65 @@ describe('The getSales Service function', () => {
     expect(sales).to.equal(salesData)
   })
 })
+
+describe('The getSale Service function', () => {
+
+  describe('when there is a sale with the selected id', () => {
+    const saleData = [
+      {
+        date: '2021-09-09T04:54:29.000Z',
+        productId: 1,
+        quantity: 2
+      },
+      {
+        date: '2021-09-09T04:54:54.000Z',
+        productId: 2,
+        quantity: 2
+      }
+    ]
+
+    const id = 1;
+  
+    before(() => {
+      sinon.stub(SalesModel, 'getSale').resolves(saleData);
+    })
+  
+    after(() => {
+      SalesModel.getSale.restore();
+    });
+  
+    it('returns an array', async () => {
+      const sale = await SalesService.getSale(id);
+  
+      expect(sale).to.be.an('array');
+      expect(Object.keys(sale[0])).to.have.lengthOf(3);
+      expect(sale[0]).to.have.property('date').that.is.a('string');
+      expect(sale[0]).to.have.property('productId').that.is.a('number');
+      expect(sale[0]).to.have.property('quantity').that.is.a('number');
+    })
+  
+    it('returns the sale with the selected id', async () => {
+      const sale = await SalesService.getSale(id);
+  
+      expect(sale).to.equal(saleData)
+    })
+  })
+
+  describe('when there is no sale with the selected id', () => {
+    const saleData = [];
+
+    const id = 4;
+  
+    before(() => {
+      sinon.stub(SalesModel, 'getSale').resolves(saleData);
+    })
+  
+    after(() => {
+      SalesModel.getSale.restore();
+    });
+  
+    it('throws a "Sale not found" error', async () => {
+      await SalesService.getSale(id).should.be.rejectedWith('Sale not found');
+    })
+  })
+})

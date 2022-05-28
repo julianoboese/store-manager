e02dfb5ca922e9cec8b0e6e55e45c5369c8e16e8
Juliano Boese <juliano.boese@gmail.com>
2022-05-28 11:24:35 -0300

    feat(app): :sparkles: let app read request body
    req3

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { productsRoutes, salesRoutes } = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();

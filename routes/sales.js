0bb174a60d4b530ebf20224784cd745fc032a2ef
Juliano Boese <juliano.boese@gmail.com>
2022-05-28 12:06:26 -0300

    feat(sales): :sparkles: create POST /sales and PUT /sales/:id routes
    req3 - COMPLETE

--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const SalesController = require('../controllers/SalesController');
+const { saleValidation } = require('../middlewares');
 
 const routes = express.Router();
 
 routes.get('/', SalesController.getSales);
 
 routes.get('/:id', SalesController.getSale);
+
+routes.post('/', saleValidation);
+
+routes.put('/:id', saleValidation);
 
 module.exports = routes;

452e3e27fe33ce467b81e549468a325fb38f112b
Juliano Boese <juliano.boese@gmail.com>
2022-05-31 21:58:53 -0300

    refactor(salesCont): :art: remove try-catch blocks

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -2,51 +2,30 @@ const SalesService = require('../services/SalesService');
 
 async function getSales(_req, res) {
   const sales = await SalesService.getSales();
-
   res.status(200).json(sales);
 }
 
-async function getSale(req, res, next) {
+async function getSale(req, res) {
   const { id } = req.params;
-
-  try {
-    const sale = await SalesService.getSale(id);
-    res.status(200).json(sale);
-  } catch (error) {
-    next(error);
-  }
+  const sale = await SalesService.getSale(id);
+  res.status(200).json(sale);
 }
 
-async function postSale(req, res, next) {
-  try {
-    const sale = await SalesService.postSale(req.body);
-    res.status(201).json(sale);    
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+async function postSale(req, res) {
+  const sale = await SalesService.postSale(req.body);
+  res.status(201).json(sale);    
 }
 
-async function putSale(req, res, next) {
+async function putSale(req, res) {
   const { id } = req.params;
-
-  try {
-    const updatedSale = await SalesService.putSale({ id, saleData: req.body });
-    res.status(200).json(updatedSale);
-  } catch (error) {
-    next(error);
-  }
+  const updatedSale = await SalesService.putSale({ id, saleData: req.body });
+  res.status(200).json(updatedSale);
 }
 
-async function deleteSale(req, res, next) {
+async function deleteSale(req, res) {
   const { id } = req.params;
-  
-  try {
-    await SalesService.deleteSale(id);
-    res.status(204).end();
-  } catch (error) {
-    next(error);
-  }
+  await SalesService.deleteSale(id);
+  res.status(204).end();
 }
 
 module.exports = {

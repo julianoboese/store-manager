aaf0e1464fa3edc599b35e1e802d86aa5ff78701
Juliano Boese <juliano.boese@gmail.com>
2022-05-31 21:58:33 -0300

    refactor(prodCont): :art: remove try-catch blocks

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -2,50 +2,30 @@ const ProductsService = require('../services/ProductsService');
 
 async function getProducts(_req, res) {
   const products = await ProductsService.getProducts();
-
   res.status(200).json(products);
 }
 
-async function getProduct(req, res, next) {
+async function getProduct(req, res) {
   const { id } = req.params;
-  
-  try {
-    const product = await ProductsService.getProduct(id);
-    res.status(200).json(product);
-  } catch (error) {
-    next(error);
-  }
+  const product = await ProductsService.getProduct(id);
+  res.status(200).json(product);
 }
 
-async function postProduct(req, res, next) {
-  try {
-    const newProduct = await ProductsService.postProduct(req.body);
-    res.status(201).json(newProduct);
-  } catch (error) {
-    next(error);
-  }
+async function postProduct(req, res) {
+  const newProduct = await ProductsService.postProduct(req.body);
+  res.status(201).json(newProduct);
 }
 
-async function putProduct(req, res, next) {
+async function putProduct(req, res) {
   const { id } = req.params;
-  
-  try {
-    const updatedProduct = await ProductsService.putProduct({ id, ...req.body });
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    next(error);
-  }
+  const updatedProduct = await ProductsService.putProduct({ id, ...req.body });
+  res.status(200).json(updatedProduct);
 }
 
-async function deleteProduct(req, res, next) {
+async function deleteProduct(req, res) {
   const { id } = req.params;
-  
-  try {
-    await ProductsService.deleteProduct(id);
-    res.status(204).end();
-  } catch (error) {
-    next(error);
-  }
+  await ProductsService.deleteProduct(id);
+  res.status(204).end();
 }
 
 module.exports = {

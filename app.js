afafc3e61a2fd91368d652383a8b02a9c9504cb4
Juliano Boese <juliano.boese@gmail.com>
2022-06-27 18:01:00 -0300

    feat(app): :sparkles: install and configure swagger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('express-async-errors');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerConfig = require('./swagger.json');
 const { errorMiddleware } = require('./middlewares');
 const { productsRoutes, salesRoutes } = require('./routes');
 
@@ -7,18 +9,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/products', productsRoutes);
+
+app.use('/sales', salesRoutes);
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use('/products', productsRoutes);
-
-app.use('/sales', salesRoutes);
-
 app.use(errorMiddleware);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
